1f0e1dd4d25b437b9adfee0915b0fd3fe6229d05
Svene <aidrecabrera@gmail.com>
2023-08-15 00:05:05 +0800

    update SveneContent.jsx

--- src/SveneContent.jsx
+++ src/SveneContent.jsx
@@ -3,17 +3,19 @@ import Introduction from "./ContentIntroduction.jsx";
 import ContentWho from "./ContentWho";
 import ContentWhat from "./ContentWhat.jsx";
 import ContentWhen from "./ContentWhen.jsx";
+import ContentWriteups from "./Works/ContentWriteups.jsx";
 
 export default class SveneContent extends Component {
   render() {
     return (
       <div className="snap-y snap-mandatory">
-      <h1 className="p-3 mx-auto inset-x-0 text-center font-black text-xl fixed bg-gradient-to-b from-black to-transparent">SVENE</h1>
+      <h1 className="p-3 mx-auto inset-x-0 text-center font-black text-xl bg-gradient-to-b from-black to-transparent">SVENE</h1>
         <div className="flex flex-col">
           <Introduction className="scroll-m-6 snap-center snap-always" />
-          <ContentWho className="scroll-m-6 snap-center snap-always" />
+          <ContentWriteups className="scroll-m-6 snap-center snap-always" />
+          {/* <ContentWho className="scroll-m-6 snap-center snap-always" />
           <ContentWhat className="scroll-m-6 snap-center snap-always" />
-          <ContentWhen />
+          <ContentWhen /> */}
         </div>
       </div>
     ); 
---
+++ src/Works/ContentWriteups.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import ContentWriteupsBlocks from './ContentWriteupsBlocks';
+
+export class ContenWriteups extends Component {
+  render() {
+    return (
+      <div>
+        <div className="h-screen ">
+          <div className="p-12 flex flex-col h-screen justify-between">
+            <div>
+              <h1 className="font-semibold font-brico tracking-widest text-2xl pb-5 text-center">
+                OUTPUTS
+              </h1>
+              <div className='showcase'>
+                <ContentWriteupsBlocks/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ContenWriteups
