82751e7c345c87a798ab188bf5c351b722ddfcc7
Svene <aidrecabrera@gmail.com>
2023-08-25 05:38:25 +0800

    update ContentIntroduction.jsx

--- src/ContentIntroduction.jsx
+++ src/ContentIntroduction.jsx
@@ -8,8 +8,8 @@ export default class Introduction extends Component {
           <h1 className=" pl-32 inherit  text-left text-10xl font-thin font-darker">
             <span className="duration-500 ease-in-out hover:font-bold tracking-wider">WELCOME,</span>
             <br />
-            <span className=" duration-500 ease-in-out hover:font-bold tracking-wider">THIS</span>{" "}
-            <span className=" duration-500 ease-in-out hover:font-bold tracking-wider">IS</span>{" "}
+            <span className=" duration-500 ease-in-out hover:font-bold hover:tracking-tight tracking-wider">THIS</span>{" "}
+            <span className=" duration-500 ease-in-out hover:font-bold hover:tracking-tight tracking-wider">IS</span>{" "}
             <span className=" duration-500 ease-in-out hover:font-bold hover:text-red-600 hover:tracking-tight tracking-wider">SVENE.</span>
           </h1>
           <div className="star absolute right-5"></div>
