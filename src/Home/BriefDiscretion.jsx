570275c9de23a71359a728b8530b3f7657bfce07
Svene <aidrecabrera@gmail.com>
2023-08-26 08:27:43 +0800

    update 5 files

---
+++ src/Home/BriefDiscretion.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class BriefDiscretion extends Component {
+  render() {
+    return (
+      <>
+        <div className='grid grid-cols-3'>
+            
+        </div>
+      </>
+    )
+  }
+}
+
+export default BriefDiscretion
