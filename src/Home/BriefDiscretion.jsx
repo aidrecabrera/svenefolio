385f14c5b5030e443d9e03fd5c788993ee3cd453
Svene <aidrecabrera@gmail.com>
2023-08-26 08:59:26 +0800

    update BriefDiscretion.jsx and SveneContent.jsx

--- src/Home/BriefDiscretion.jsx
+++ src/Home/BriefDiscretion.jsx
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class BriefDiscretion extends Component {
   render() {
     return (
       <>
-        <div className='grid grid-cols-3'>
-            
+        <div className="grid grid-cols-3 gap-4">
+          <div className=""></div>
+          <div className=" font-darker col-span-2 pt-60 pb-60 text-left">
+            <h1 className="text-9xl font-thin">CURRENT STATUS</h1>
+            <p className="text-5xl pr-72 font-extralight uppercase">
+              I find myself in a phase of exploration, uncertainty, exploration,
+              and discovery.
+            </p>
+          </div>
         </div>
       </>
-    )
+    );
   }
 }
 
-export default BriefDiscretion
+export default BriefDiscretion;
--- src/SveneContent.jsx
+++ src/SveneContent.jsx
@@ -5,20 +5,24 @@ import ContentWhat from "./ContentWhat.jsx";
 import ContentWhen from "./ContentWhen.jsx";
 import ContentWrite from "./Writeups/ContentWrite.jsx";
 import ContentProject from "./Projects/ContentProject.jsx";
-import BackgroundEffect from "./Misc/BackgroundEffect"
-import BackgroundGradient from "./Misc/BackgroundGradient.jsx"
-import CursorOverlay from "./Misc/CursorOverlay.jsx"
+import BackgroundEffect from "./Misc/BackgroundEffect";
+import BackgroundGradient from "./Misc/BackgroundGradient.jsx";
+import CursorOverlay from "./Misc/CursorOverlay.jsx";
 import BackgroundTest from "./Misc/BackgroundTest.jsx";
 import BriefIntroduction from "./Home/BriefIntroduction.jsx";
+import BriefDiscretion from "./Home/BriefDiscretion.jsx";
 export default class SveneContent extends Component {
   render() {
     return (
       <div className="snap-y snap-mandatory">
         <div className="flex flex-col">
           <Introduction className="scroll-m-6 snap-center snap-always" />
-          <BriefIntroduction/>
+          <div className="h-screen">
+            <BriefIntroduction />
+            <BriefDiscretion />
+          </div>
         </div>
       </div>
-    ); 
+    );
   }
 }
